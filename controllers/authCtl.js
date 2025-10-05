81ba3cd112659279712950775e5fac25a5bbc879
HungDuong2909 <hungduongg2909@gmail.com>
2025-10-05 12:01:05 +0700

    sendgrid api

--- controllers/authCtl.js
+++ controllers/authCtl.js
@@ -2,9 +2,13 @@ const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
 require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
 
 const Account = require("../models/Account");
 const Token = require("../models/Token");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 // const createMailTransporter = () => require("../utils/mailer");
 
 // const SMTP_HOST = process.env.SMTP_HOST || "smtp.gmail.com";
@@ -306,30 +310,9 @@ exports.forgotPasswordCtl = async (req, res) => {
       const resetPasswordUrl = `${domain}/reset-password?token=${token}`;
 
       // Cấu hình email
-      const SMTP_HOST = process.env.SMTP_HOST || "smtp.sendgrid.net";
-      const SMTP_PORT = Number(process.env.SMTP_PORT || 587); // 587=STARTTLS, 465=SMTPS
-      const SMTP_USER = process.env.SMTP_USER || "apikey"; // SendGrid yêu cầu 'apikey'
-      const SMTP_PASS = process.env.SMTP_PASS; // API key 'SG....'
-      const IS_SECURE = SMTP_PORT === 465;
-
-      const transporter = nodemailer.createTransport({
-         host: SMTP_HOST,
-         port: SMTP_PORT,
-         secure: IS_SECURE, // 465 -> true, 587 -> false
-         auth: { user: SMTP_USER, pass: SMTP_PASS },
-         connectionTimeout: 10000, // tránh treo
-         socketTimeout: 10000,
-         logger: process.env.NODE_ENV !== "production",
-         debug: process.env.NODE_ENV !== "production",
-         tls: { minVersion: "TLSv1.2" },
-      });
-
       const mailOptions = {
-         from: {
-            name: "No-reply Forgot Password Embroidery",
-            address: process.env.FROM_EMAIL || "noreply@yourdomain.com",
-         },
          to: email,
+         from: process.env.FROM_EMAIL || "noreply@yourdomain.com",
          subject: "Đặt lại mật khẩu - Embroidery",
          html: `
                   <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
@@ -355,7 +338,7 @@ exports.forgotPasswordCtl = async (req, res) => {
       };
 
       // Gửi email
-      await transporter.sendMail(mailOptions);
+      await sgMail.send(mailOptions);
 
       res.status(200).json({
          success: true,
